a17baa5cc99275f248c2ae3fcde6e7981bfe96d2
Brien Hartung <brien.hartung@outlook.com>
2017-09-29 20:36:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-var theCanvas = document.getElementById("canvasOne");
+window.addEventListener("load", eventWindowLoaded, false);
+function eventWindowLoaded () {
+   canvasApp();
+}
+
+var CanvasApp = {
+  var theCanvas = document.getElementById("canvasOne");
   var context = theCanvas.getContext("2d");
-
-context.strokeStyle = "#000000"
-
-            context.globalAlpha = 1;
-            context.fillStyle = "#000000";
-              context.fillRect(0, 0, 640, 480);
+  
+  context.fillStyle = "#aaaaff";     // light blue
+  context.fillRect(0, 0, 400, 400);
+}
